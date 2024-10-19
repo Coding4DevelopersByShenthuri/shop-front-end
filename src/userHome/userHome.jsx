6babb7705ce45ee2fca14d404df7370a018cba59
niroshan <iamsniroshan@gmail.com>
2024-10-19 19:28:21 +0530

    fixed

--- src/userHome/userHome.jsx
+++ src/userHome/userHome.jsx
@@ -1,10 +1,4 @@
 import React from 'react'
-import Banner from '../components/Banner'
-import BestSellerProducts from './BestSellerProducts'
-import FavProduct from './FavProduct'
-import PromoBanner from './PromoBanner'
-import OtherProducts from './OtherProducts'
-import Review from './Review'
 
 const userHome = () => {
   return (
