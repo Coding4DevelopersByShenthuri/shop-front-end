054e0602e8bab60fa806c2b7552526dc761b5ba7
niroshan <iamsniroshan@gmail.com>
2024-10-22 16:50:59 +0530

    done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -42,7 +42,7 @@ const Login = () => {
         loginWithGoogle()
             .then((result) => {
                 const user = result.user;
-                const userData = { email: user.email, birthday: '', name:'' };
+                const userData = { email: user.email, birthday: '', name:user.displayName  };
                 fetch(`http://localhost:3000/user/createuser/${user.uid}`, {
                     method: 'POST',
                     headers: {
